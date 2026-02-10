b454495d0fefe8639ef6c7021f2ba0aa9f0f7cee
anupagaire <aanupa.gaire@gmail.com>
2026-02-10 20:45:22 +0545

    Fix nested Router issue in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,19 @@
-import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./components/Home"; 
 import Certificates from "./components/Certificates";
-import Footer from "./components/Footer"
+import Footer from "./components/Footer";
 
 function App() {
   return (
-    <Router>
+    <>
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/certificates" element={<Certificates />} />
       </Routes>
-      <Footer/>
-    </Router>
+      <Footer />
+    </>
   );
 }
 
